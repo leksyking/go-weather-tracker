d1d4c23bd5458de7b23127ae71f7efc7b8629ffc
Leksyking <gbemilekeogundipe@gmail.com>
2022-08-29 05:56:19 +0100

    load the api config key

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type apiConfigData struct {
 	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
 }
@@ -9,4 +14,18 @@ type WeatherData struct {
 	Main struct {
 		Kelvin float64 `json:"temp"`
 	} `json:"main"`
+}
+
+func LoadApiConfig(filename string) (apiConfigData, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+
+	var c apiConfigData
+	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+	return c, nil
 }
