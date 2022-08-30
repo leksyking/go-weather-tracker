d72fda68f36949ff68148aaa4344be19d16970a1
Leksyking <gbemilekeogundipe@gmail.com>
2022-08-30 01:05:27 +0100

    handle the routes and integrate the open weather api

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 type apiConfigData struct {
@@ -28,4 +30,45 @@ func LoadApiConfig(filename string) (apiConfigData, error) {
 		return apiConfigData{}, err
 	}
 	return c, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!, from Go!\n"))
+}
+
+func query(city string) (WeatherData, error) {
+	apiConfig, err := LoadApiConfig(".apiConfig")
+	if err != nil {
+		return WeatherData{}, err
+	}
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?appid=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+	if err != nil {
+		return WeatherData{}, err
+	}
+	defer resp.Body.Close()
+	var d WeatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return WeatherData{}, err
+	}
+	return d, nil
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
